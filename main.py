261d28595886205ff45a8df17874f57f3d40be99
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-03-12 14:15:11 +0000

    added setup.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this to your needs
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
